66618257a66a7930ad7077832a88b9a0515e3f8b
Prasanth S <prasanth.snk@gmail.com>
2024-06-09 07:34:56 +0530

    changes

--- autorun.bat
+++ autorun.bat
@@ -10,5 +10,3 @@ python youtube_m3ugrabber_tamil.py > ../tamil.m3u
 python youtube_m3ugrabber_kids.py > ../kids.m3u
 
 python youtube_m3ugrabber_sports.py > ../sports.m3u
-
-python fix_m3u.py > ../fix_m3u.m3u
--- autorun.sh
+++ autorun.sh
@@ -14,6 +14,4 @@ python3 youtube_m3ugrabber_tamil.py > ../tamil.m3u
 
 python3 youtube_m3ugrabber_kids.py > ../kids.m3u
 
-python3 fix_m3u.py > ../fix_m3u.m3u
-
 echo m3u grabbed
