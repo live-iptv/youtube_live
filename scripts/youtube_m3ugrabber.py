635d7498409304aad3353e052159c30ef65edd5f
Prasanth S <prasanth.snk@gmail.com>
2024-09-18 21:00:01 +0530

    changes

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -16,8 +16,7 @@ with open('../youtube_channel_info.txt') as f:
             tvg_id = line[3].strip()
             print(f'\n#EXTINF:-1 group-title="{grp_title}" tvg-logo="{tvg_logo}" tvg-id="{tvg_id}", {ch_name}')
         else:
-            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-            response = s.get(line, headers=headers, timeout=15, verify=False).text
+            response = s.get(line, timeout=15).text
             m3u8_links = re.findall(r'https://[^"]+\.m3u8', response)
             if m3u8_links:
                 link = m3u8_links[0] 
