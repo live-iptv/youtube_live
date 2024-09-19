ab5fb1f2c41dbfa4e9f76fe0c287c048fcf1c197
Prasanth S <prasanth.snk@gmail.com>
2024-09-19 17:04:19 +0530

    changes

--- scripts/youtube_m3ugrabber_tamil.py
+++ scripts/youtube_m3ugrabber_tamil.py
@@ -16,8 +16,7 @@ with open('../youtube_channel_info_tamil.txt') as f:
             tvg_id = line[3].strip()
             print(f'\n#EXTINF:-1 group-title="{grp_title}" tvg-logo="{tvg_logo}" tvg-id="{tvg_id}", {ch_name}')
         else:
-            response = s.get('https://corsproxy.io/{line}', timeout=15).text
-            print(response)
+            response = s.get(line, timeout=15).text
             m3u8_links = re.findall(r'https://[^"]+\.m3u8', response)
             if m3u8_links:
                 link = m3u8_links[0] 
