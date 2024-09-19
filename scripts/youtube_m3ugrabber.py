8a8d5d9bd3a797109632b270649b06e0a48de8b5
Prasanth S <prasanth.snk@gmail.com>
2024-09-19 11:58:34 +0530

    changes

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -1,27 +1,10 @@
-import http.client
-import re
-# Specify the URL and path
-url = 'www.youtube.com'
-path = '/channel/UCup3etEdjyF1L3sRbU-rKLw/live'
+import urllib.request
 
-# Create a connection
-conn = http.client.HTTPSConnection(url, timeout=15)
+url = 'https://www.youtube.com/channel/UCup3etEdjyF1L3sRbU-rKLw/live'
 
 try:
-    # Make the request
-    conn.request("GET", path)
-    
-    # Get the response
-    response = conn.getresponse()
-    
-    # Read and decode the response
-    data = response.read().decode('utf-8')
-    
-    m3u8_links = re.findall(r'https://[^"]+\.m3u8', data)
-    if m3u8_links:
-        link = m3u8_links[0] 
-    else:
-        link = 'https://live-iptv.github.io/youtube_live/assets/info.m3u8'                    
-    print(link)
-finally:
-    conn.close()
+    with urllib.request.urlopen(url, timeout=15) as response:
+        data = response.read().decode('utf-8')
+        print(data)
+except Exception as e:
+    print(f"An error occurred: {e}")
