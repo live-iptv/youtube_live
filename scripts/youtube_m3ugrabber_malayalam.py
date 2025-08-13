1ac5238e4cee6195d82923c92594742ea746486a
Prasanth S <prasanth.snk@gmail.com>
2025-08-13 19:57:38 +0530

    changes

--- scripts/youtube_m3ugrabber_malayalam.py
+++ scripts/youtube_m3ugrabber_malayalam.py
@@ -3,6 +3,10 @@ import re
 
 print('#EXTM3U')
 s = requests.Session()
+headers = {
+    'Accept': 'application/json',
+    'User-Agent': 'Mozilla/5.0 (compatible; Python script)'
+}
 with open('../youtube_channel_info_malayalam.txt') as f:
     for line in f:
         line = line.strip()
@@ -16,7 +20,7 @@ with open('../youtube_channel_info_malayalam.txt') as f:
             tvg_id = line[3].strip()
             print(f'\n#EXTINF:-1 group-title="{grp_title}" tvg-logo="{tvg_logo}" tvg-id="{tvg_id}", {ch_name}')
         else:
-            response = s.get(line, timeout=15).text
+            response = s.get(line, headers=headers, timeout=15).text
             m3u8_links = re.findall(r'https://[^"]+\.m3u8', response)
             if m3u8_links:
                 link = m3u8_links[0] 
